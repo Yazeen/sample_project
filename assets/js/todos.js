5389b511de65e2f8de2dfaf0b115f62eed1f8aa9
Yazeen <yaz33n@live.com>
2018-08-20 23:00:43 +0530

    Completed Project

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -19,3 +19,20 @@ $("input[type='text']").keypress( function( e ) {
 $(".fa-plus-square").on("click", function() {
 	$("input[type='text']").fadeToggle(500);
 });
+
+
+//cd <dir>                             		       //Navigate.
+//git init 										   //Set the folder as git.
+//git status                                	   //View files inside of the git folder.
+//git add <filename>                         	   //Add files to backup staging part. (-A <filename> to add all files)
+//git commit -m "message"                     	   //Commit and save changes.
+//git checkout -- .                           	   //Replace current files/folders (if any) with the backed up commited files/folders.
+//git clone https://github.com/<link>/project.git  //Download and set current directory as git init from the given link's files.
+//git remote -v                                    //Check the fetch and push url of git.
+//git remote set-url origin <link>                 //Set the url of ur github directory.
+//git push origin master                           //Upload the files and folders to your github page.
+
+//node <filename.js>                        
+//npm install <filename> -save 
+//npm init
+
